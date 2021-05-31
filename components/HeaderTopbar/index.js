70911344c98e4a80e4147826dc05a2a81e8549e5
Noah Bresler <noahbresler2002@gmail.com>
2021-05-31 14:03:05 -0500

    Clean up top bar

--- components/HeaderTopbar/index.js
+++ components/HeaderTopbar/index.js
@@ -1,34 +1,34 @@
 import React from "react";
+
 import Link from "next/link";
+
+import { PhoneIcon, MailIcon } from "@heroicons/react/outline";
 
 const HeaderTopbar = () => {
   return (
-    <div className="topbar">
-      <div className="container">
-        <div className="row">
-          <div className="col col-md-6 col-sm-12 col-12">
-            <div className="contact-intro">
-              <ul>
-                <li>
-                  <i className="fi flaticon-call"></i>+000123456789
-                </li>
-                <li>
-                  <i className="fi flaticon-envelope"></i> nasarna@gmail.com
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="col col-md-6 col-sm-12 col-12">
-            <div className="contact-info">
-              <ul>
-                <li>
-                  <Link href="/DonatePage">
-                    <a className="theme-btn">Donate Now</a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
+    <div className="w-full border-b border-gray-200">
+      <div className="flex flex-row items-center justify-between max-w-3xl m-auto ">
+        <ul className="flex flex-row h-full">
+          <li className="flex flex-row items-center mr-3">
+            {/* TODO Make this link a phone number */}
+            <a href="#">
+              <PhoneIcon className="inline w-4 h-4 text-gray-500" />
+              <span>+000123456789</span>
+            </a>
+          </li>
+          <span className="block w-[1px] bg-gray-200 h-auto"></span>
+          <li className="flex flex-row items-center ml-3">
+            {/* TODO Make this link an email */}
+            <a href="#">
+              <MailIcon className="inline w-4 h-4 text-gray-500 mr-1 transform" />
+              <span>nasarna@gmail.com</span>
+            </a>
+          </li>
+        </ul>
+        <div className="px-3 py-2">
+          <Link href="/DonatePage">
+            <a className="btn-main">Donate!</a>
+          </Link>
         </div>
       </div>
     </div>
