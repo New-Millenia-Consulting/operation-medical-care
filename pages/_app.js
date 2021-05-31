f0d4fde90333776eb3208d4406cac61566403ab8
Noah Bresler <noahbresler2002@gmail.com>
2021-05-31 14:29:19 -0500

    Uninstall bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/themify-icons.css";
 import "../styles/flaticon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/react-modal-video/scss/modal-video.scss";
 import "../styles/globals.css";
 import "../styles/style.css";
