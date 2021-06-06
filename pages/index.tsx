4ce5eb81ece3a684eb583b40744e8c4cec55a772
Noah Bresler <noahbresler2002@gmail.com>
2021-06-06 10:39:39 -0500

    Remove unnecessary sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,28 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "../components/header";
 import Hero from "../components/hero";
 import Mission from "../components/mission";
 import About from "../components/about";
-import CaseSlide from "../components/case";
 import CounterSection from "../components/counter";
 import TeamSection from "../components/team";
-import WorldSection from "../components/world";
-import BlogSection from "../components/BlogSection";
 import Footer from "../components/footer";
-import Scrollbar from "../components/scrollbar";
 import Head from "next/head";
 
 const HomePage = () => {
   return (
-    <Fragment>
+    <>
       <Head>
         <title>Operation Medical Care</title>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <Header />
       <Hero />
       <Mission />
       <About />
-      <CaseSlide />
       <CounterSection />
       <TeamSection />
-      <WorldSection />
-      <BlogSection />
       <Footer />
-      <Scrollbar />
-    </Fragment>
+    </>
   );
 };
 export default HomePage;
