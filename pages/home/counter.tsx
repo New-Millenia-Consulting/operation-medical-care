238e1f6d08f15db506b202d398f442e7becdbc73
Noah Bresler <noahbresler2002@gmail.com>
2021-06-14 04:56:16 -0500

    Responsive counter

--- pages/home/counter.tsx
+++ pages/home/counter.tsx
@@ -2,7 +2,7 @@ const CounterSection = () => {
   return (
     <div className="grid py-20 bg-primary-800">
       <div className="m-auto max-w-screen-2xl">
-        <div className="grid w-full grid-cols-4 gap-12 divide-x divide-gray-50 divide-opacity-50">
+        <div className="relative grid w-full grid-cols-2 gap-12 lg:divide-x lg:grid-cols-4 lg:divide-gray-50 lg:divide-opacity-50">
           <div className="flex flex-col items-center">
             <span className="text-6xl font-bold text-gray-50">$40k+</span>
             <span className="text-primary-200">In Donations</span>
@@ -19,6 +19,8 @@ const CounterSection = () => {
             <span className="text-6xl font-bold text-gray-50">11k+</span>
             <span className="text-primary-200">Patients Treated</span>
           </div>
+          <div className="lg:hidden absolute w-full h-px bg-primary-300 top-[50%] left-0"></div>
+          <div className="lg:hidden absolute w-px h-full bg-primary-300 left-[50%] top-0"></div>
         </div>
       </div>
     </div>
