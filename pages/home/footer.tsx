bc84b2cc310d11680ec4e50fa49395a0fbb0add0
Noah Bresler <noahbresler2002@gmail.com>
2021-06-14 05:43:02 -0500

    Edit footer

--- pages/home/footer.tsx
+++ pages/home/footer.tsx
@@ -1,12 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center pt-12 bg-primary-700">
-      <h2 className="text-3xl font-semibold text-white">
-        Want To Keep Updated?
-        <p className="my-10 text-center text-white">TBD</p>
-      </h2>
-
-      <div className="flex items-center justify-center w-full py-3 text-white bg-primary-800">
+    <footer className="flex flex-col items-center bg-blue-gray-50">
+      <div
+        className="flex items-center justify-center w-full max-w-5xl py-5 mx-3 text-gray-500 border-t border-gray-200"
+        style={{ width: "80%" }}
+      >
         &copy; 2021 Operation Medical Care. All Rights Reserved
       </div>
     </footer>
