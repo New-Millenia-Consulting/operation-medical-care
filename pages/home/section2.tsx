5fdb65fc2e661b42890642d7acfd5ade8b1e3243
zac chester <zacchesterpiano@gmail.com>
2021-06-15 17:50:12 -0400

    added new section

--- pages/home/section2.tsx
+++ pages/home/section2.tsx
@@ -1,34 +1,111 @@
-import React from 'react';
-import { Parallax, Background } from 'react-parallax';
-
-const Section2 = () => (
-  <div>
-    <Parallax
-      blur={10}
-      bgImage="/images/home/hero/dreamstime__xxl_114898539.jpg"
-      bgImageAlt="the cat"
-      strength={200}
-    >
-      Put some text content here
-      or even an empty div with fixed dimensions
-      to have a height for the parallax.
-      <div style={{ height: '200px' }} />
-    </Parallax>
-    <Parallax
-      blur={{ min: -15, max: 15 }}
-      bgImage="/images/home/hero/dreamstime__xxl_114898539.jpg"
-      bgImageAlt="the dog"
-      strength={200}
-    >
-      Blur transition from min to max
-      <div style={{ height: '200px' }} />
-    </Parallax>
-    <Parallax strength={300}>
-      <div>Use the background component for custom elements</div>
-      <Background className="custom-bg">
-        <img src="/images/home/hero/dreamstime__xxl_114898539.jpg" alt="" />
-      </Background>
-    </Parallax>
-  </div>
-);
-export default Section2;
+const stats = [
+    { label: 'Founded', value: '2021' },
+    { label: 'Employees', value: '5' },
+    { label: 'Beta Users', value: '521' },
+    { label: 'Raised', value: '$25M' },
+  ]
+  
+  export default function section2() {
+    return (
+      <div className="relative py-16 bg-white sm:py-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
+          <div className="relative sm:py-16 lg:py-0">
+            <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+              <div className="absolute inset-y-0 w-full right-1/2 bg-gray-50 rounded-r-3xl lg:right-72" />
+              <svg
+                className="absolute -ml-3 top-8 left-1/2 lg:-right-8 lg:left-auto lg:top-12"
+                width={404}
+                height={392}
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+              </svg>
+            </div>
+            <div className="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+              {/* Testimonial card*/}
+              <div className="relative pt-64 pb-10 overflow-hidden shadow-xl rounded-2xl">
+                <img
+                  className="absolute inset-0 object-cover w-full h-full"
+                  src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
+                  alt=""
+                />
+                <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
+                <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
+                <div className="relative px-8">
+                  <div>
+                    <img
+                      className="h-12"
+                      src="/images/home/hero/dreamstime_xxl_114898439.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <blockquote className="mt-8">
+                    <div className="relative text-lg font-medium text-white md:flex-grow">
+                      <svg
+                        className="absolute top-0 left-0 w-8 h-8 text-indigo-400 transform -translate-x-3 -translate-y-2"
+                        fill="currentColor"
+                        viewBox="0 0 32 32"
+                        aria-hidden="true"
+                      >
+                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                      </svg>
+                      <p className="relative">
+                        Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
+                        Montes, magna cursus nulla feugiat dignissim id lobortis amet.
+                      </p>
+                    </div>
+  
+                    <footer className="mt-4">
+                      <p className="text-base font-semibold text-indigo-200">Sarah Williams, CEO at Workcation</p>
+                    </footer>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          <div className="relative max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:px-0">
+            {/* Content area */}
+            <div className="pt-12 sm:pt-16 lg:pt-20">
+              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                On a mission To<br></br>Help Those In&nbsp;Need
+              </h2>
+              <div className="mt-6 space-y-6 text-gray-500">
+                <p className="text-lg">
+                Every year, we spent 8 days traveling to remote villages in
+              Belize. Our team of medical doctors, nurses, pharmacists,
+              dietians, business owners, software engineers, and kind everday
+              volunteers, treat over 900 patients and distribute much needed
+              supplies to improve the lives of those in need. We not only
+              provide vital medical necessities, but hats—to prevent glaucoma,
+              toys, school supplies, and everyday items that would otherwise be&nbsp;unaffordable.
+                </p>
+              </div>
+            </div>
+  
+        
+              <div className="mt-10">
+                <a href="#" className="text-base font-medium text-indigo-600">
+                  {' '}
+                  Learn more about how we're changing the world <span aria-hidden="true">&rarr;</span>{' '}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+    )
+  }
