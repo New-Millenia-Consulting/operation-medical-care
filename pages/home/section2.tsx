137e954855d823438dbcfb28658c9a3036de6546
Noah Bresler <noahbresler2002@gmail.com>
2021-06-16 06:10:20 -0500

    Remove question mark from file name

--- pages/home/section2.tsx
+++ pages/home/section2.tsx
@@ -1,95 +1,105 @@
 const stats = [
-    { label: 'Founded', value: '2021' },
-    { label: 'Employees', value: '5' },
-    { label: 'Beta Users', value: '521' },
-    { label: 'Raised', value: '$25M' },
-  ]
-  
-  export default function section2() {
-    return (
-      <div className="relative py-16 bg-white sm:py-24">
-        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
-          <div className="relative sm:py-16 lg:py-0">
-            <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-              <div className="absolute inset-y-0 w-full right-1/2 bg-gray-50 rounded-r-3xl lg:right-72" />
-              <svg
-                className="absolute -ml-3 top-8 left-1/2 lg:-right-8 lg:left-auto lg:top-12"
-                width={404}
-                height={392}
-                fill="none"
-                viewBox="0 0 404 392"
-              >
-                <defs>
-                  <pattern
-                    id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+  { label: "Founded", value: "2021" },
+  { label: "Employees", value: "5" },
+  { label: "Beta Users", value: "521" },
+  { label: "Raised", value: "$25M" },
+];
+
+export default function section2() {
+  return (
+    <div className="relative py-16 bg-white sm:py-24">
+      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
+        <div className="relative sm:py-16 lg:py-0">
+          <div
+            aria-hidden="true"
+            className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen"
+          >
+            <div className="absolute inset-y-0 w-full right-1/2 bg-gray-50 rounded-r-3xl lg:right-72" />
+            <svg
+              className="absolute -ml-3 top-8 left-1/2 lg:-right-8 lg:left-auto lg:top-12"
+              width={404}
+              height={392}
+              fill="none"
+              viewBox="0 0 404 392"
+            >
+              <defs>
+                <pattern
+                  id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect
                     x={0}
                     y={0}
-                    width={20}
-                    height={20}
-                    patternUnits="userSpaceOnUse"
-                  >
-                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                  </pattern>
-                </defs>
-                <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
-              </svg>
-            </div>
-            <div className="relative max-w-md px-4 mx-auto border-0 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
-              {/* Testimonial card*/}
-              <div className="relative pt-64 pb-10 overflow-hidden border-0 shadow-xl rounded-2xl">
-                <img
-                  className="absolute inset-0 object-cover w-full h-full border-0"
-                  src="/images/home/hero/OMC-?-Belize-Comms-blog-&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
-                  alt=""
-                />
-                <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
-                <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
-                <div className="relative px-8 border-0">
-                  <div>
-                    <img
-                      className="h-12"
-                      src="/images/home/hero/dreamstime_xxl_114898439.jpg"
-                      alt=""
-                    />
-                  </div>
-                 
-  
-                  
-                 
+                    width={4}
+                    height={4}
+                    className="text-gray-200"
+                    fill="currentColor"
+                  />
+                </pattern>
+              </defs>
+              <rect
+                width={404}
+                height={392}
+                fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)"
+              />
+            </svg>
+          </div>
+          <div className="relative max-w-md px-4 mx-auto border-0 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+            {/* Testimonial card*/}
+            <div className="relative pt-64 pb-10 overflow-hidden border-0 shadow-xl rounded-2xl">
+              <img
+                className="absolute inset-0 object-cover w-full h-full border-0"
+                src="/images/home/hero/OMC-Belize-Comms-blog-&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
+                alt=""
+              />
+              <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
+              <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
+              <div className="relative px-8 border-0">
+                <div>
+                  <img
+                    className="h-12"
+                    src="/images/home/hero/dreamstime_xxl_114898439.jpg"
+                    alt=""
+                  />
                 </div>
               </div>
             </div>
           </div>
-  
-          <div className="relative max-w-md px-4 mx-auto border-0 sm:max-w-3xl sm:px-6 lg:px-0">
-            {/* Content area */}
-            <div className="pt-12 sm:pt-16 lg:pt-20">
-              <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                On a mission To<br></br>Help Those In&nbsp;Need
-              </h2>
-              <div className="mt-6 space-y-6 text-gray-500">
-                <p className="text-lg">
+        </div>
+
+        <div className="relative max-w-md px-4 mx-auto border-0 sm:max-w-3xl sm:px-6 lg:px-0">
+          {/* Content area */}
+          <div className="pt-12 sm:pt-16 lg:pt-20">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              On a mission To<br></br>Help Those In&nbsp;Need
+            </h2>
+            <div className="mt-6 space-y-6 text-gray-500">
+              <p className="text-lg">
                 Every year, we spent 8 days traveling to remote villages in
-              Belize. Our team of medical doctors, nurses, pharmacists,
-              dietians, business owners, software engineers, and kind everday
-              volunteers, treat over 900 patients and distribute much needed
-              supplies to improve the lives of those in need. We not only
-              provide vital medical necessities, but hats—to prevent glaucoma,
-              toys, school supplies, and everyday items that would otherwise be&nbsp;unaffordable.
-                </p>
-              </div>
-            </div>
-  
-        
-              <div className="mt-10">
-                <a href="#" className="text-base font-medium text-indigo-600">
-                  {' '}
-                  Learn more about how we're changing the world <span aria-hidden="true">&rarr;</span>{' '}
-                </a>
-              </div>
+                Belize. Our team of medical doctors, nurses, pharmacists,
+                dietians, business owners, software engineers, and kind everday
+                volunteers, treat over 900 patients and distribute much needed
+                supplies to improve the lives of those in need. We not only
+                provide vital medical necessities, but hats—to prevent glaucoma,
+                toys, school supplies, and everyday items that would otherwise
+                be&nbsp;unaffordable.
+              </p>
             </div>
           </div>
+
+          <div className="mt-10">
+            <a href="#" className="text-base font-medium text-indigo-600">
+              {" "}
+              Learn more about how we're changing the world{" "}
+              <span aria-hidden="true">&rarr;</span>{" "}
+            </a>
+          </div>
         </div>
-    
-    )
-  }
+      </div>
+    </div>
+  );
+}
