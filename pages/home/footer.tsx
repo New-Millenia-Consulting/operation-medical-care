1b2d5c55cbb9212978128bc7b00242344147ed80
Vishnu Sriram <vsriram42@gmail.com>
2023-02-03 19:41:28 -0500

    Changed footer copyright to 2023

--- pages/home/footer.tsx
+++ pages/home/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center bg-mainGreen">
       <div className="flex items-center justify-center w-4/5 max-w-5xl py-5 mx-3 text-xs text-center text-gray-500 md:text-base">
-        &copy; 2021 Operation Medical Care. <br className="sm:hidden" />
+        &copy; 2023 Operation Medical Care. <br className="sm:hidden" />
         All Rights Reserved
       </div>
     </footer>
